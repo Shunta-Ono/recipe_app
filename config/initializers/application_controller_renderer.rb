066161ea4a32b59e8e0c3de45a9cae0bb510e6b8
Shunta-Ono <nisikoubaseball.a@softbank.ne.jp>
2020-05-16 01:11:58 +0000

    [create]user_edit,update

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,5 @@
 #     https: false
 #   )
 # end
+
+Refile.secret_key = 'ce4f4477b0fed24d4baa0e3ec960830f603634e53b3787f3875defceef1846c4cb69129ec76716b1c11d10aff0b77ea057fe20271368b96aa82dfcce7179f55b'
